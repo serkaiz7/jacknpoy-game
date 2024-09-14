f63f7ff20d94be25967bb896c9297bf8b9d812c4
serkaiz7 <163306095+serkaiz7@users.noreply.github.com>
2024-09-14 23:43:54 +0800

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,23 +1,11 @@
-// js/firebase.js
-
-// Import Firebase modules (if using modules)
-// Otherwise, include Firebase scripts in HTML
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC56LawqZV4vhslnp6pQLhXD3UxGYST9vY",
+  apiKey: "AIzaSyC56LawqZV4vhslnp6pQLhXD3UxGYST9vY",
     authDomain: "jack-n-poy.firebaseapp.com",
     databaseURL: "https://jack-n-poy-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "jack-n-poy",
     storageBucket: "jack-n-poy.appspot.com",
     messagingSenderId: "706617302358",
-    appId: "1:706617302358:web:bc1573c1350cda5b1c4af7",
-    measurementId: "G-N2FRK3JL2F"
+    appId: "1:706617302358:web:bc1573c1350cda5b1c4af7"
 };
-
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Initialize services
 const auth = firebase.auth();
-const db = firebase.database();
